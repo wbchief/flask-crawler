d3b2449fb92a249ea0f87590ea6282e363e46e1d
chief <712640388@qq.com>
2018-10-20 15:32:38 +0800

    封装成类，并保存到数据库mysql，mongodb

---
+++ app/crawlers/ebookspider.py
@@ -0,0 +1,189 @@
+import re
+import random
+
+import requests
+import time
+from pyquery import PyQuery as pq
+from requests import RequestException
+
+from app import db
+from app.crawlers.novelsql import get_list, novels, SqlDB
+from app.models import Ebooks
+
+base_url = 'https://www.qu.la'
+url = 'https://www.qu.la/paihangbang/'
+pattern = re.compile(u'(第?([一二三四五六七八九零十百千万亿]+|[0-9]+)?章)')
+IPs = [
+        {'HTTPS': 'https://115.237.16.200:8118'},
+        {'HTTPS': 'https://42.49.119.10:8118'},
+        {'HTTPS': 'http://60.174.74.40:8118'}
+    ]
+headers = {
+    # 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    # 'Accept-Encoding': 'gzip, deflate, br',
+    # 'Accept-Language': 'zh-CN,zh;q=0.9',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}
+
+class EbookSpider:
+    def __init__(self):
+
+        self.data = {}
+        self.sql = SqlDB()
+
+    def get_allbook_url(self):
+        '''
+        获取e笔趣阁
+        :return: 首页所有书链接，保存到mongodb all_book集合
+        '''
+        try:
+            all_book = get_list('all_book')
+            ip = random.choice(IPs)
+            response = requests.get(url, headers=headers, proxies=ip)
+            if response.status_code == 200:
+                html = response.content.decode('utf-8')
+                doc = pq(html)
+                items = doc('#main div.mbottom').items()
+                count = 1
+                for item in items:
+                    lis = item.find('div:nth-child(2) > div:nth-child(1) > ul > li').items()
+                    for li in lis:
+                        count += 1
+                        href = li.find('a').attr('href')
+                        book_url = base_url + href
+                        book_name = li.find('a').text().strip()
+                        data = {
+                            'book_name': book_name,
+                            'book_url': book_url
+                        }
+                        all_book.insert_one(data)
+                        count += 1
+        except Exception as e:
+            print(e.args)
+            self.get_allbook_url()
+
+    def get_chapter_url(self, book_url, book_name):
+
+        temp_data = {
+            'book_name': book_name,
+            'book_url': book_url
+        }
+        try:
+            ip = random.choice(IPs)
+            response = requests.get(book_url, headers=headers, proxies=ip)
+            if response.status_code == 200:
+                html = response.content.decode('utf-8')
+                doc = pq(html)
+                div_info = doc('#maininfo div#info')
+                # name = div_info.find('h1').text().strip()
+                author = div_info.find('p:nth-child(2)').text().replace('作  者：', '').strip()
+                # print(author)
+                last_since = div_info.find('p:nth-child(4)').text().replace('最后更新：', '').strip()
+                new_chapter = div_info.find('p:nth-child(5) > a').text().strip()
+                about_book = doc('#maininfo div#intro').text().strip()
+                image_url = base_url + doc('#fmimg > img').attr('src')
+                self.data['book_name'] = book_name
+                self.data['author'] = author
+                self.data['book_image'] = None
+                self.data['last_since'] = last_since
+                self.data['new_chapter'] = new_chapter
+                self.data['about_book'] = about_book
+                self.data['book_url'] = book_url
+                print(self.data)
+                # 章节
+                list_name = author + '-' + book_name
+                list_name_db = get_list(list_name)
+                self.sql.insert_data(self.data)
+                # fail_book_db
+                num = 1
+                for a in doc('div.box_con div#list > dl > dd:gt(11) > a').items():
+                    # print(num)
+                    chapter_url = base_url + a.attr('href')
+                    chapter_name = a.text()
+                    # print(chapter_name)
+                    data1 = {
+                        "_id": num,
+                        "chapter_name": chapter_name,
+                        "chapter_url": chapter_url
+                    }
+                    list_name_db.insert_one(data1)
+                    num += 1
+                success_book = get_list('success_book')
+                success_book.insert_one(self.data)
+                all_book = get_list('all_book')
+                all_book.delete_one({ 'book_name': book_name,
+                                      'book_url': book_url})
+                # print(book_name, '所有目录链接已近爬取完毕------------------------')
+                return author
+        except RequestException as e:
+            # 爬取book失败则将book_url放入失败列表
+            print(e.args)
+            fail_book_db = get_list('fail_book')
+            fail_book_db.insert_one(temp_data)
+            # print(book_name, '爬取目录链接失败------------------------')
+        except Exception as e:
+            print(e.args)
+            print(book_name, '已爬取')
+
+    def get_chapter_content(self, chapter_success_list, chapter_fail_list, chapter_url, id, chapter_name):
+
+        success_db = get_list(chapter_success_list)
+        fail_db = get_list(chapter_fail_list)
+        data = {}
+        data['num'] = id
+        data['chapter_url'] = chapter_url
+        data['chapter_name'] = chapter_name
+        try:
+            ip = random.choice(IPs)
+            response = requests.get(chapter_url, headers=headers, proxies=ip)
+            if response.status_code == 200:
+                html = response.content.decode('utf-8')
+                doc = pq(html)
+                content = doc('#content').text()
+                self.data['content'] = content
+                success_db.insert_one(data)
+                print(id, chapter_name, '爬取完毕')
+        except RequestException as e:
+            print(e.args)
+            fail_db.insert_one(data)
+        except Exception as e:
+            print(e.args)
+            print(chapter_name, '爬去失败')
+
+    def srart_spider(self):
+        steps = 100
+        starttime = time.time()
+        # 获取全部小说的链接
+        self.get_allbook_url()
+        all_book_db = novels.get_collection('all_book')
+        count = 1
+        for book in all_book_db.find():
+            book_name = book.get('book_name')
+            book_url = book.get('book_url')
+            print('开始爬取%s' % book_name, '----------------------------')
+            author = self.get_chapter_url(book_url, book_name)
+            chapter_fail_list = author + '-' + book_name + '-fail'
+            chapter_success_list = author + '-' + book_name + '-success'
+            for bb in novels.list_collection_names():
+                if 'book' in bb:
+                    continue
+                book_db = get_list(bb)
+                for col in book_db.find():
+                    id = col.get('_id')
+                    chapter_url = col.get('chapter_url')
+                    chapter_name = col.get('chapter_name')
+                    self.get_chapter_content(chapter_success_list, chapter_fail_list, chapter_url, id, chapter_name)
+            print(book_name, '爬取完毕--------------------')
+            print('\n' * 5)
+            time.sleep(1)
+            count += 1
+        print(count)
+        endtime = time.time()
+        print(endtime - starttime)
+
+
+if __name__ == '__main__':
+    spider = EbookSpider()
+    spider.srart_spider()
+    #get_chapter_content('https://www.qu.la/book/17069/6826072.html')
+
