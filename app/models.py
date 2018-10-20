3d7ae402bf4fbb8a825e87561db5babfd4bbcc3c
chief <712640388@qq.com>
2018-10-20 14:28:54 +0800

    删除表

--- app/models.py
+++ app/models.py
@@ -14,28 +14,28 @@ class Ebooks(db.Model):
     new_chapter = db.Column(db.String(128)) # 最新章节
     about_book = db.Column(db.Text) #  简介
     book_url = db.Column(db.String(128)) #书链接
-    chapter = db.relationship('Chapter', backref='ebook', lazy='dynamic')
+    #chapter = db.relationship('Chapter', backref='ebook', lazy='dynamic')
 
 
-class Chapter(db.Model):
-    '''
-    章节表
-    '''
-    __tablename__ = 'chapters'
-    id = db.Column(db.Integer, primary_key=True)
-    chapter_name = db.Column(db.String(128))# 章节名
-    chapter_url = db.Column(db.String(128))#章节url
-    # 外键
-    book_id = db.Column(db.Integer, db.ForeignKey('ebooks.id'))
-    chapter_content = db.relationship('Content', backref='chapter', lazy='dynamic')
-
-
-class Content(db.Model):
-    '''
-    章节内容
-    '''
-    __tablename__ = 'contents'
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.Text)
-    chapter_id = db.Column(db.Integer, db.ForeignKey('chapters.id'))
+# class Chapter(db.Model):
+#     '''
+#     章节表
+#     '''
+#     __tablename__ = 'chapters'
+#     id = db.Column(db.Integer, primary_key=True)
+#     chapter_name = db.Column(db.String(128))# 章节名
+#     #chapter_url = db.Column(db.String(128))#章节url
+#     # 外键
+#     book_id = db.Column(db.Integer, db.ForeignKey('ebooks.id'))
+#     chapter_content = db.relationship('Content', backref='chapter', lazy='dynamic')
+#
+#
+# class Content(db.Model):
+#     '''
+#     章节内容
+#     '''
+#     __tablename__ = 'contents'
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.Text)
+#     chapter_id = db.Column(db.Integer, db.ForeignKey('chapters.id'))
 
