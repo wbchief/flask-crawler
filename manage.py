d90b264a126bf0acc39be0462a88af579e75c794
chief <712640388@qq.com>
2018-10-20 14:28:13 +0800

    删除contents，chapter表

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Shell
 
 from app import create_app, db
-from app.models import Content, Chapter, Ebooks
+from app.models import Ebooks
 
 app = create_app('development')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, Ebook=Ebooks, Chapter=Chapter, Content=Content)
+    return dict(app=app, db=db, Ebook=Ebooks)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
